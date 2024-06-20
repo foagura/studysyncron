45d60e4938b7ef2d94029fa6a931fb5855351ecd
foagura <foagura_0416@yahoo.co.jp>
2024-06-20 21:27:10 +0900

    user バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :image
+
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\Z/i
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には英語と数字両方を含めてください' }
 end
