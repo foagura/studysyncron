4b22d99f36676d2b263e90f6cb0703327d34a3f6
foagura <foagura_0416@yahoo.co.jp>
2024-07-04 22:29:37 +0900

    markdown_helperの修正

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,24 +1,28 @@
 module MarkdownHelper
   require 'rouge/plugins/redcarpet'
 
-  class CustomRender < Redcarpet::Render::Base
+  class CustomRender < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
   end
 
   def markdown(text)
+    render_options = {
+      filter_html:         true, # ユーザーが入力したhtmlを出力しない
+      hard_wrap:           true, # 改行をhtmlの<br>に置き換え
+      space_after_headers: true,  # ヘッダー記号(#)と文字の間にスペース必要
+      link_attributes: { rel: 'nofollow', taget: '_blank' },
+      fenced_code_blocks: true
+    }
+
     options = {
       no_intra_emphasis: true,
       tables: true,
-      fenced_code_blocks: true,
       autolink: true,
       lax_spacing: true,
-      space_after_headers: true,
-      hard_wrap: true,
-      link_attributes: { rel: 'nofollow', taget: '_blank' },
       quote: true
     }
 
-    renderer = CustomRender.new
+    renderer = CustomRender.new(render_options)
     markdown = Redcarpet::Markdown.new(renderer, options)
     markdown.render(text).html_safe
   end
