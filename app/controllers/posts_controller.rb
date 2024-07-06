185294e95b2f38004460e237dec78a6cdea84c51
foagura <foagura_0416@yahoo.co.jp>
2024-07-06 17:48:14 +0900

    edit_actionへの未ログイン時の制限実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_post, only: [:show, :edit, :update]
   before_action :user_confirmation, only: [:edit]
 
