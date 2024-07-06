ab2533fbacc77ecb17bd6df9417d2431ab3ea73d
foagura <foagura_0416@yahoo.co.jp>
2024-07-06 17:11:42 +0900

    投稿編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -19,7 +20,17 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -27,5 +38,9 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:study_hours, :study_minutes, 
                                  :title, :content, :start_time).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
