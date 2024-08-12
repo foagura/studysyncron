47a944f47ca5fe02057903697aeebdf9d0ff7532
foagura <foagura_0416@yahoo.co.jp>
2024-08-12 21:13:48 +0900

    新規投稿できない場合

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   has_many_attached :images
 
   with_options presence: true do
-    validates :study_hours, :study_minutes, numericality: { only_integer: true }
+    validates :study_hours, numericality: { only_integer: true, in: 0..23 }
+    validates :study_minutes, numericality: { only_integer: true, in: 0..59 }
     validates :title, :content, :start_time
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,10 +13,55 @@ RSpec.describe Post, type: :model do
     end
 
     context '新規投稿できない場合' do
+      it 'userに紐づいていないと登録できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Userを入力してください")
+      end
       it 'titleが空だと登録できない' do
         @post.title = ""
         @post.valid?
         expect(@post.errors.full_messages).to include("タイトルが入力されていません。")
+      end
+      it 'contentが空だと登録できない' do
+        @post.content = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("内容が入力されていません。")
+      end
+      it 'start_timeが空だと登録できない' do
+        @post.start_time = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("登録時刻が入力されていません。")
+      end
+      it 'study_hoursが空だと登録できない' do
+        @post.study_hours = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("学習時間(時間)が入力されていません。")
+      end
+      it 'study_minutesが空だと登録できない' do
+        @post.study_minutes = ""
+        @post.valid?
+        expect(@post.errors.full_messages).to include("学習時間(分)が入力されていません。")
+      end
+      it 'study_hoursが数字以外だと登録できない' do
+        @post.study_hours = "テスト"
+        @post.valid?
+        expect(@post.errors.full_messages).to include("学習時間(時間)は数値で入力してください")
+      end
+      it 'study_minutesが数字以外だと登録できない' do
+        @post.study_minutes = "テスト"
+        @post.valid?
+        expect(@post.errors.full_messages).to include("学習時間(分)は数値で入力してください")
+      end
+      it 'study_hoursが0~23以外の数字だと登録できない' do
+        @post.study_hours = 100
+        @post.valid?
+        expect(@post.errors.full_messages).to include("学習時間(時間)は0..23の範囲に含めてください")
+      end
+      it 'study_minutesが0~59以外の数字だと登録できない' do
+        @post.study_minutes = 100
+        @post.valid?
+        expect(@post.errors.full_messages).to include("学習時間(分)は0..59の範囲に含めてください")
       end
     end
   end
