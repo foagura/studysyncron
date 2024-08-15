345c8be72b89d505085971c42a84bdea4d30e166
foagura <foagura_0416@yahoo.co.jp>
2024-08-15 23:56:04 +0900

    migration~association記述まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :comments
   has_one_attached :image
 
   validates :nickname, presence: true
